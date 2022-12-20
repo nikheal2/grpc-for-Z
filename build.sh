38d7583adb5a10457ab989d9f1ff3727b52fb26f
Modassar Rana <modassar.rana@ibm.com>
2022-12-20 14:47:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
+echo $1
 python3.9 -m pip install --upgrade pip
-GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1 python3 -m pip install --no-cache-dir --user grpcio==${GRPC_LATEST}
-echo "Building grpc ${{ inputs.release }}"
+GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1 python3 -m pip install --no-cache-dir --user grpcio==$1
