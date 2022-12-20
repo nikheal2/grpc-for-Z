ebadbaf1603b72f9086af830e2cb9ff4d99e459e
Modassar Rana <modassar.rana@ibm.com>
2022-12-20 16:08:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo $1
 python3.9 -m pip install --upgrade pip
-GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1 python3 -m pip install --no-cache-dir --user grpcio==$1
+export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
+python3 -m pip install --no-cache-dir --user grpcio==$1
