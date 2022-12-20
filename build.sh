3da4fec55f551921f1713ff8f85866fdd1d94f89
Modassar Rana <modassar.rana@ibm.com>
2022-12-20 19:04:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 echo $1
 python3.9 -m pip install --upgrade pip
 export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
-python3 -m pip install --no-cache-dir --user grpcio==$1
+echo " Openssl is set to true : $GRPC_PYTHON_BUILD_SYSTEM_OPENSSL"
+python3 -m pip install grpcio==$1
