daf4756a26e022751efae185e427c2062608a8cc
Modassar Rana <modassar.rana@ibm.com>
2022-12-22 11:48:12 +0530

    Update build.sh
    Added openssl & openssl-devel package

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo $1
 python3.9 -m pip install --upgrade pip
+apt-get install -y openssl openssl-devel
 export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
-echo " Openssl is set to true : $GRPC_PYTHON_BUILD_SYSTEM_OPENSSL"
 python3 -m pip install grpcio==$1
