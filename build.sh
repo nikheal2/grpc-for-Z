e011fc82dcee15dcaeb91a30f35e341647237631
Modassar Rana <modassar.rana@ibm.com>
2023-01-23 17:52:59 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo $1
-sudo python3.9 -m pip3 install --upgrade pip3
-sudo apt-get install -y openssl libssl-dev
+python3.9 -m pip3 install --upgrade pip3
+apt-get install -y openssl libssl-dev
 export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
-sudo python3.9 -m pip3 install grpcio==$1 --no-binary=grpcio
+python3.9 -m pip3 install grpcio==$1 --no-binary=grpcio
