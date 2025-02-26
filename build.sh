59b7799064671b82d81519271107ef39dca3dc6e
Nikhil Suryawanshi <77109245+nikheal2@users.noreply.github.com>
2025-02-26 15:11:04 +0530

    Update python version to 3.11

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ python3.11 -m pip install --upgrade pip
 apt-get install -y openssl libssl-dev python3.9-distutils
 export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
 export GRPC_PYTHON_BUILD_WITH_CYTHON=1
-python3.9 -m pip install grpcio==$1 --no-cache-dir
+python3.11 -m pip install grpcio==$1 --no-cache-dir
