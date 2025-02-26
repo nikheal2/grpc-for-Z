0a1807bb2b4f5088c613daf7ef1cddcf5a4f5e34
Nikhil Suryawanshi <77109245+nikheal2@users.noreply.github.com>
2025-02-26 15:10:09 +0530

    Update python version to 3.11

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo $1
-python3.9 -m pip install --upgrade pip
+python3.11 -m pip install --upgrade pip
 apt-get install -y openssl libssl-dev python3.9-distutils
 export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
 export GRPC_PYTHON_BUILD_WITH_CYTHON=1
